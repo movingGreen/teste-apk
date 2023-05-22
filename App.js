31bbfb24cd8b7dbc1499bf0faa91674ccf7a5a6b
Elizeu <el_vailant@hotmail.com>
2023-05-22 15:11:13 -0400

    tesete

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
 import { StyleSheet, Text, View, Platform} from 'react-native';
 import React, { useState, useEffect } from "react";
-import MapView, { UrlTile } from "react-native-maps";
+import MapView, { Polygon, Circle, UrlTile} from "react-native-maps";
 import * as Location from 'expo-location';
 
 const App = () => {
 
   const urlTile = "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png";
   
+  const poligon = [
+    {
+     latitude: -55.992610677887086,
+     longitude: -15.501319502227588
+    },
+    {
+     latitude: -56.02165326627755,
+     longitude: -15.52819553456699
+    },
+    {
+     latitude: -56.01820266171589,
+     longitude: -15.543987033553066
+    },
+    {
+     latitude: -55.98340906572389,
+     longitude: -15.558946284401983
+    },
+    {
+     latitude: -55.94832791935099,
+     longitude: -15.545649226205555
+    },
+    {
+     latitude: -55.96356808949662,
+     longitude: -15.511294520543359
+    },
+    {
+     latitude: -55.992610677887086,
+     longitude: -15.501319502227588
+    }
+  ]
+
   const [location, setLocation] = useState({
-    latitude: 50,
-    longitude: 50,
+    latitude: -56.00957615031356,
+    longitude: -15.499102766068347,
     latitudeDelta: 0.0001,
     longitudeDelta: 0.0001,
   });
@@ -25,6 +56,7 @@ const App = () => {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      console.log(JSON.stringify(location.coords));
     })();
   }, []);
   
@@ -34,17 +66,25 @@ const App = () => {
       <MapView
         showsMyLocationButton={true}
         style={styles.map} 
-        region={location}
         showsUserLocation={true}
         zoomEnabled={true}
         loadingEnabled={true}
-        // mapType={Platform.OS == "android" ? "none" : "standard"}
+        mapType={Platform.OS == "android" ? "none" : "standard"}
       >
         <UrlTile
           urlTemplate={urlTile} 
           maximumZ={15}
           flipY={false}
         />
+        <Polygon 
+          coordinates={poligon}
+        />
+        <Circle 
+          center={{
+            latitude: -15.499102766068347,
+            longitude: -56.00957615031356
+          }}
+          radius={500}/>
       </MapView>
     </View>
   );
