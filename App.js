a9f2ab961c46f718eeb05da77a17cda3dbb1bc40
Elizeu <el_vailant@hotmail.com>
2023-05-22 09:09:40 -0400

    teste-apk

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform} from 'react-native';
+import React, { useState, useEffect } from "react";
+import MapView, { UrlTile } from "react-native-maps";
+import * as Location from 'expo-location';
 
-export default function App() {
+const App = () => {
+
+  const urlTile = "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  
+  const [location, setLocation] = useState({
+    latitude: 50,
+    longitude: 50,
+    latitudeDelta: 0.0001,
+    longitudeDelta: 0.0001,
+  });
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        showsMyLocationButton={true}
+        style={styles.map} 
+        region={location}
+        showsUserLocation={true}
+        zoomEnabled={true}
+        loadingEnabled={true}
+        // mapType={Platform.OS == "android" ? "none" : "standard"}
+      >
+        <UrlTile
+          urlTemplate={urlTile} 
+          maximumZ={15}
+          flipY={false}
+        />
+      </MapView>
     </View>
   );
 }
@@ -17,4 +57,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: "100%",
+    height: "100%"
+  }
 });
+
+export default App;
